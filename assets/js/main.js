73ac6518752aa92279a8d95e8ad2bccbaad975a7
Shailesh Dhandhukiya <shailesh@ektasitechnology.com>
2023-03-01 22:44:30 +0530

    header & Footer modified by new design scheme

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,32 +8,78 @@
 (function ($) {
     "use strict";
 
-    // mobile nav script
-    var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
-          $(".navbar-nav > li  a").each(function(){
-          if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
-          $(this).addClass("active");
-          // $(this).parent("li").addClass("active");
-        })
+    /*=============================================
+        =    		Mobile Menu			      =
+    =============================================*/
+    //SubMenu Dropdown Toggle
+    if ($('.menu-area li.menu-item-has-children ul').length) {
+        $('.menu-area .navigation li.menu-item-has-children').append('<div class="dropdown-btn"><span class="fas fa-angle-down"></span></div>');
+    }
 
-// ------------------------ Navigation Scroll
-        $(window).on('scroll', function (){   
-            var sticky = $('.sticky-menu'),
-            scroll = $(window).scrollTop();
-            if (scroll >= 100) sticky.addClass('fixed');
-            else sticky.removeClass('fixed');
-  
-          });
+    //Mobile Nav Hide Show
+    if ($('.mobile-menu').length) {
 
-          // -------------------- Remove Placeholder When Focus Or Click
-        $("input,textarea").each( function(){
-            $(this).data('holder',$(this).attr('placeholder'));
-            $(this).on('focusin', function() {
-                $(this).attr('placeholder','');
-            });
-            $(this).on('focusout', function() {
-                $(this).attr('placeholder',$(this).data('holder'));
-            });     
+        var mobileMenuContent = $('.menu-area .main-menu').html();
+        $('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
+
+        //Dropdown Button
+        $('.mobile-menu li.menu-item-has-children .dropdown-btn').on('click', function () {
+            $(this).toggleClass('open');
+            $(this).prev('ul').slideToggle(300);
         });
+        //Menu Toggle Btn
+        $('.mobile-nav-toggler').on('click', function () {
+            $('body').addClass('mobile-menu-visible');
+        });
+
+        //Menu Toggle Btn
+        $('.menu-backdrop, .mobile-menu .close-btn').on('click', function () {
+            $('body').removeClass('mobile-menu-visible');
+        });
+    }
+
+
+    /*=============================================
+        =     Menu sticky & Scroll to top      =
+    =============================================*/
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < 245) {
+            $("#sticky-header").removeClass("sticky-menu");
+            $('.scroll-to-target').removeClass('open');
+
+        } else {
+            $("#sticky-header").addClass("sticky-menu");
+            $('.scroll-to-target').addClass('open');
+        }
+    });
+
+    /*=============================================
+        =    		Button Effect  	         =
+    =============================================*/
+    $('.btn').on('mouseenter', function (e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({ top: relY, left: relX })
+    }).on('mouseout', function (e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({ top: relY, left: relX })
+    });
+
+    /*=============================================
+        =          header btn active               =
+    =============================================*/
+    $(function () {
+        $(".header-btn").on('click', function () {
+            $('.header-contact-wrap, .body-contact-overlay').toggleClass("active");
+            $('body').toggleClass("fix");
+        });
+        $(".body-contact-overlay").on('click', function () {
+            $('.header-contact-wrap, .body-contact-overlay').removeClass("active");
+        });
+    });
 
 });
